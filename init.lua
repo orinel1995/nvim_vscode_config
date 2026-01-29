0cd538bae892aeba78482d8f6cc41ebbba96fffa
Aleksandr Kondratyev <orinel12@jandex.ru>
2026-01-29 09:03:01 +0300

    vim arrows

--- init.lua
+++ init.lua
@@ -7,30 +7,21 @@
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.opt.clipboard = "unnamedplus"
 
 local vim = vim
 
--- ------------------------
 -- Keymaps
--- ------------------------
+
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+local modes = { "n", "v", "o" }
 
 map("i", "jk", "<Esc>", { silent = true })
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
-local opts = { noremap = true, silent = true }
-local modes = { "n", "v", "o" }
-
-vim.keymap.set(modes, "j", "h", opts)  -- j = left
-vim.keymap.set(modes, "k", "k", opts)  -- k = up
-vim.keymap.set(modes, "l", "l", opts)  -- l = right
-vim.keymap.set(modes, ",", "j", opts)  -- , = down
-
-vim.opt.clipboard = "unnamedplus"
--- ------------------------
 -- Explorer
--- ------------------------
 
 -- Переключение на файловый менеджер (Explorer)
 map('n', '<leader>e', function()
@@ -59,21 +50,17 @@ map({'n','v','i'}, '<C-l>', function()
   vim.fn['VSCodeNotify']('workbench.action.focusRightGroup')
 end, { silent = true })
 
--- ------------------------
--- Tabs (documents) in current split
--- ------------------------
 -- Следующий открытый документ
 map('n', '<leader>l', function()
   vim.fn['VSCodeNotify']('workbench.action.nextEditor')
 end, { silent = true })
+
 -- Предыдущий открытый документ
 map('n', '<leader>j', function()
   vim.fn['VSCodeNotify']('workbench.action.previousEditor')
 end, { silent = true })
 
--- ------------------------
 -- Lazy.nvim bootstrap
--- ------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -87,9 +74,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- ------------------------
 -- Plugin setup
--- ------------------------
+
 require("lazy").setup({
 	-- Core non-conflicting plugins
 	{ "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup({}) end },
@@ -108,6 +94,7 @@ require("lazy").setup({
 			})
 		end
 	},
+
 	-- Color schemes
 	{ "catppuccin/nvim", as = "catppuccin" },
 	{ "ellisonleao/gruvbox.nvim", as = "gruvbox" },
@@ -122,9 +109,8 @@ require("lazy").setup({
 	{ "folke/twilight.nvim" },              -- dim surrounding
 })
 
--- ------------------------
 -- Retain config requires
--- ------------------------
+
 require("config.options")
 
 vim.defer_fn(function()
